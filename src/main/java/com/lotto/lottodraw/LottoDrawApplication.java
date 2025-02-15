555e4679b79e4cafd3a2c4a4072775bb7c2bb52f
jeongggggg <ahjeong921@gmail.com>
2025-02-15 10:00:00 +0900

    Setting: 초기 커밋

---
+++ src/main/java/com/lotto/lottodraw/LottoDrawApplication.java
@@ -0,0 +1,13 @@
+package com.lotto.lottodraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoDrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoDrawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lotto/lottodraw/LottoDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotto.lottodraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoDrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
