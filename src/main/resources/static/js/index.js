f0b7bd51f641b54fa6be7eb20834af8b18818ff6
jeongggggg <ahjeong921@gmail.com>
2025-02-19 11:30:00 +0900

    Refactor: 프론트 화면 디자인 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const resultContainer = document.getElementById("result");
+    resultContainer.textContent = "기대하시라 ~ 🤹🏻";
+});
+
 document.getElementById("drawButton").addEventListener("click", function () {
     fetch("http://localhost:8080/lotto")
         .then(response => response.json())
         .then(data => {
-            document.getElementById("result").innerText = "당첨 번호: " + data.join(", ");
+            const resultContainer = document.getElementById("result");
+            resultContainer.innerHTML = "";
+
+            data.forEach(number => {
+                const ball = document.createElement("div");
+                ball.classList.add("ball");
+
+                const numberSpan = document.createElement("span");
+                numberSpan.textContent = number;
+
+                if (number <= 10) {
+                    ball.style.background = "#fbc400";
+                } else if (number <= 20) {
+                    ball.style.background = "#69c8f2";
+                } else if (number <= 30) {
+                    ball.style.background = "#ff7272";
+                } else if (number <= 40) {
+                    ball.style.background = "#aaa";
+                } else {
+                    ball.style.background = "#b0d840";
+                }
+
+                ball.appendChild(numberSpan);
+                resultContainer.appendChild(ball);
+            });
         })
         .catch(error => console.error("에러 발생:", error));
 });
